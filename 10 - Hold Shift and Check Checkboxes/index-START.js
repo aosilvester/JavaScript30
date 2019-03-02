fc9fd27c0535828e6fbceb630786977c6b94b495
Alex <38813232+aosilvester@users.noreply.github.com>
2019-03-02 13:00:18 -0500

    day 10, hold shift to check multiple checkboxes

---
+++ 10 - Hold Shift and Check Checkboxes/index-START.js
@@ -0,0 +1,27 @@
+const checkboxes = document.querySelectorAll('.inbox input[ type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e){
+    // check if they had the shift key down
+    // &&& check if they are unchecking it
+    let inBetween = false;
+    if (e.shiftKey && this.checked){
+        // go ahead and do what we please. sausage gets made
+        // here we loop over every single checkbox
+        checkboxes.forEach(checkbox => {
+            console.log(checkbox)
+            if (checkbox === this || checkbox === lastChecked){
+                inBetween = !inBetween;
+            }
+
+            if (inBetween){
+                checkbox.checked = true;
+            }
+        })
+    }
+
+    lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck))
